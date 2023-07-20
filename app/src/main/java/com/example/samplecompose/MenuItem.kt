22e5f6a65aa68bdfacc912f339ac5ab3f512803d
mehdi <dehghani_m@rocketmail.com>
2023-07-20 19:33:06 +0330

    make navigation drawer

--- app/src/main/java/com/example/samplecompose/MenuItem.kt
+++ app/src/main/java/com/example/samplecompose/MenuItem.kt
@@ -5,5 +5,6 @@ import androidx.compose.ui.graphics.vector.ImageVector
 data class MenuItem(
     val id: String,
     val title: String,
+    val contentDescription: String,
     val icon: ImageVector
 )
--- app/src/main/java/com/example/samplecompose/NavigationDrawer.kt
+++ app/src/main/java/com/example/samplecompose/NavigationDrawer.kt
@@ -1,12 +1,18 @@
 package com.example.samplecompose
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -22,4 +28,30 @@ fun DrawerHeader(){
         
     }
     
+}
+
+@Composable
+fun DrawerBody(
+    items: List<MenuItem>,
+    modifier: Modifier = Modifier,
+    itemTextStyle: TextStyle = TextStyle(fontSize = 18.sp),
+    onItemClick: (MenuItem) -> Unit
+){
+    LazyColumn (modifier) {
+        items(items){ item ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        onItemClick(item)
+                    }
+                    .padding(16.dp)
+            ) {
+                Icon(imageVector = item.icon, contentDescription = null)
+
+            }
+
+        }
+    }
+
 }
