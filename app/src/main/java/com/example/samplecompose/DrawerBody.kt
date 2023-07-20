ba2b8a34fc80c3188a8de44de8566baa4fe0aaf5
mehdi <dehghani_m@rocketmail.com>
2023-07-20 17:58:34 +0330

    make another page compose

---
+++ app/src/main/java/com/example/samplecompose/DrawerBody.kt
@@ -0,0 +1,31 @@
+package com.example.samplecompose
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.CoroutineScope
+
+@Composable
+fun DrawerBody(
+    menuItems: List<MenuItem>,
+    scaffoldState: ScaffoldState,
+    scope: CoroutineScope,
+    modifier: Modifier = Modifier,
+    onItemClick: (MenuItem) -> Unit
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(menuItems) { item ->
+            DrawerItem(
+                item,
+                modifier = modifier
+            ) {
+                scope.launch {
+                    scaffoldState.drawerState.close()
+                }
+                onItemClick(item)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/samplecompose/ScreenContent.kt
@@ -0,0 +1,22 @@
+package com.example.samplecompose
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+
+@Composable
+fun ScreenContent(titleId: Int, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = stringResource(id = titleId), style = MaterialTheme.typography.headlineLarge)
+    }
+}
