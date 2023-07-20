c20e0c081aff643baaa6742bf0584d4efe0322db
mehdi <dehghani_m@rocketmail.com>
2023-07-20 13:22:50 +0330

    start make project

--- app/src/main/java/com/example/samplecompose/MainActivity.kt
+++ app/src/main/java/com/example/samplecompose/MainActivity.kt
@@ -3,13 +3,27 @@ package com.example.samplecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.samplecompose.ui.theme.SampleComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +36,52 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    greeting()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    SampleComposeTheme {
-        Greeting("Android")
+fun greeting(){
+    Box(modifier = Modifier.fillMaxSize()){
+
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentSize(align = Alignment.TopStart)){
+            Image(painter = painterResource(R.drawable.baseline_menu_24),
+                contentDescription = null,
+                modifier =
+                Modifier
+                    .padding(8.dp)
+                    .size(50.dp)
+            )
+        }
+
+        val drawerState = rememberDrawerState(DrawerValue.Closed)
+        val scope = rememberCoroutineScope()
+
     }
+
+
 }
+
+@Composable
+fun drawerMenu(
+    isDrawerOpen : Boolean,
+    onToggleDrawer: () -> Unit
+){
+    Box(modifier = Modifier.fillMaxSize()){
+
+        if(isDrawerOpen){
+            
+        }
+    }
+
+}
+
+
+
