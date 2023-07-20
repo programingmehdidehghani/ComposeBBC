d0a3b95231d5c8a9ef2c3c50a09b71d5bba7f6fc
mehdi <dehghani_m@rocketmail.com>
2023-07-20 17:53:52 +0330

    make strings

--- app/src/main/java/com/example/samplecompose/MainActivity.kt
+++ app/src/main/java/com/example/samplecompose/MainActivity.kt
@@ -4,26 +4,42 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.example.samplecompose.ui.theme.SampleComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,52 +52,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    greeting()
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Preview(showBackground = true)
-@Composable
-fun greeting(){
-    Box(modifier = Modifier.fillMaxSize()){
-
-        Box(modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentSize(align = Alignment.TopStart)){
-            Image(painter = painterResource(R.drawable.baseline_menu_24),
-                contentDescription = null,
-                modifier =
-                Modifier
-                    .padding(8.dp)
-                    .size(50.dp)
-            )
-        }
-
-        val drawerState = rememberDrawerState(DrawerValue.Closed)
-        val scope = rememberCoroutineScope()
-
-    }
 
 
-}
 
-@Composable
-fun drawerMenu(
-    isDrawerOpen : Boolean,
-    onToggleDrawer: () -> Unit
-){
-    Box(modifier = Modifier.fillMaxSize()){
 
-        if(isDrawerOpen){
-            
-        }
-    }
 
-}
 
 
 
