ae75a78d3e97a13fb31f80d8f3524d4e3bc06120
mehdi <dehghani_m@rocketmail.com>
2023-07-20 19:51:27 +0330

    try make app bar

--- app/src/main/java/com/example/samplecompose/NavigationDrawer.kt
+++ app/src/main/java/com/example/samplecompose/NavigationDrawer.kt
@@ -3,8 +3,10 @@ package com.example.samplecompose
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Icon
@@ -47,8 +49,13 @@ fun DrawerBody(
                     }
                     .padding(16.dp)
             ) {
-                Icon(imageVector = item.icon, contentDescription = null)
-
+                Icon(imageVector = item.icon, contentDescription = item.contentDescription)
+                Spacer(modifier = Modifier.width(16.dp))
+                Text(
+                    text = item.title,
+                    style = itemTextStyle,
+                    modifier = Modifier.weight(1f)
+                )
             }
 
         }
