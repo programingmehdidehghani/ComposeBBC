449c36fefe5ae284bee8fac6b86783e3c0def542
mehdi <dehghani_m@rocketmail.com>
2023-07-20 19:15:17 +0330

    start mmake data class

--- app/src/main/java/com/example/samplecompose/NavigationDrawer.kt
+++ app/src/main/java/com/example/samplecompose/NavigationDrawer.kt
@@ -3,18 +3,22 @@ package com.example.samplecompose
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun DrawerHeader(){
     Box(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
             .padding(vertical = 4.dp),
         contentAlignment = Alignment.Center
     ) {
+        Text(text = "Header", fontSize = 60.sp)
         
     }
     
