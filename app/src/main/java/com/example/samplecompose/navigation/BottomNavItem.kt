579db206239aeb7e311a620dc40c1f4e61cc517e
mehdi <dehghani_m@rocketmail.com>
2023-07-25 14:39:27 +0330

    try make selad class

--- app/src/main/java/com/example/samplecompose/navigation/BottomNavItem.kt
+++ app/src/main/java/com/example/samplecompose/navigation/BottomNavItem.kt
@@ -1,5 +1,31 @@
 package com.example.samplecompose.navigation
 
-sealed class BottomNavItem(
+import com.example.samplecompose.R
+import com.example.samplecompose.util.Constants
 
-)
+sealed class BottomNavItem(
+    val title: String,
+    val image: Int,
+    val route: String
+){
+    object Agents: BottomNavItem(
+        title = Constants.CATEGORY_AGENTS,
+        image = R.drawable.ic_agents,
+        route = Screen.Agents.route
+    )
+    object Maps: BottomNavItem(
+        title = Constants.CATEGORY_MAPS,
+        image = R.drawable.ic_maps,
+        route = Screen.Maps.route
+    )
+    object Weapons: BottomNavItem(
+        title = Constants.CATEGORY_WEAPONS,
+        image = R.drawable.ic_weapons,
+        route = Screen.Weapons.route
+    )
+    object Tiers: BottomNavItem(
+        title = Constants.CATEGORY_COMPETITIVE_TIERS,
+        image = R.drawable.ic_tiers,
+        route = Screen.CompetitiveTiers.route
+    )
+}
